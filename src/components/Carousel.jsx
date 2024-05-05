41bda262d238c9d3898d39f9e3a9fb820f033566
im-sanny <raselparvezsanny@gmail.com>
2024-05-05 11:48:24 +0600

    banner added

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,47 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Slide from "./Slide";
+import bgimg1 from '../assets/images/carousel1.jpg'
+import bgimg2 from '../assets/images/carousel2.jpg'
+import bgimg3 from '../assets/images/carousel3.jpg'
+
+export default function Carousel() {
+  return (
+    <div className="container px-2  lg:px-12 mx-auto">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Slide image={bgimg1}></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide image={bgimg2}></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide image={bgimg3}></Slide>
+        </SwiperSlide>
+        
+      </Swiper>
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
   return (
-    <div className="navbar bg-base-100 shadow-sm container px-4 mx-auto">
+    <div className="navbar bg-base-100 shadow-sm container px-12 mx-auto">
       <div className="flex-1">
         <div className="flex gap-2 items-center">
           <img className="w-auto h-7" src={logo} alt="" />
