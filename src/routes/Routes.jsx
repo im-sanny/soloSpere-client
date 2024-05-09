8d02a9204eb87329df3d4b9b7b113b445c4f0e77
im-sanny <raselparvezsanny@gmail.com>
2024-05-09 13:27:21 +0600

    added addjob page succesfully

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,14 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home";
 import Login from "../pages/autentication/Login";
-import Register from "../pages/autentication/Registration";
 import Registration from "../pages/autentication/Registration";
 import JobDetails from "../pages/JobDetails";
+import AddJob from "../pages/AddJob";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         index: true,
@@ -29,6 +31,10 @@ const router = createBrowserRouter([
         loader: ({ params }) =>
           fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`),
       },
+      {
+        path:'/add-job',
+        element:<AddJob></AddJob>,
+      }
     ],
   },
 ]);
