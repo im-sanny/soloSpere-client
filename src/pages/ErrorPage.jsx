bd6f5f766a107b07b69020cb848c722e21fbf35e
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2024-12-20 21:07:30 +0600

    add: added option to redirect to the previous page when the page show u an error

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
+  const handleBackPage = () => {
+    window.history.back();
+  };
   return (
     <section className="bg-white ">
       <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
@@ -15,7 +18,10 @@ const ErrorPage = () => {
           </p>
 
           <div className="flex items-center mt-6 gap-x-3">
-            <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100 ">
+            <button
+              onClick={handleBackPage}
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100 "
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -34,7 +40,10 @@ const ErrorPage = () => {
               <span>Go back</span>
             </button>
 
-            <Link to={'/'} className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-gray-500 rounded-lg shrink-0 sm:w-auto hover:bg-gray-600">
+            <Link
+              to={'/'}
+              className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-gray-500 rounded-lg shrink-0 sm:w-auto hover:bg-gray-600"
+            >
               Take me home
             </Link>
           </div>
